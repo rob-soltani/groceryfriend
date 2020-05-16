7f6790bc3a5868c00937f41c7fe211c53881d0e7
Sohrab Soltani - Sohrab-Dell Win 10 <rob@soltani.io>
2020-05-16 16:49:42 -0400

    Cloud functions and rewritews

--- FireBase App/functions/index.js
+++ FireBase App/functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+// const firebase = require('firebase-admin');
+const express = require ('express');
+const engines = require('consolidate');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// const firebaseApp = firebase.initializeApp(
+//     functions.config().firebase
+// );
+
+
+const app = express();
+app.engine('hbs', engines.handlebars);
+app.set('views', './views');
+app.set('view engine', 'hbs');
+
+
+app.get('/api/login', (request, response) => 
+{ 
+    response.send(`${Date.now()}`);
+});
+
+
+
+exports.app = functions.region('us-east1').https.onRequest(app);
