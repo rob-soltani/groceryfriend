4d97ca6dd2870f4e92180e5c13555a49f27056e7
Rob Soltani @ Rob-Dell <rob@soltani.io>
2020-10-16 20:32:11 -0400

    Preparing for Selenium presentation for CCPS 406

--- FireBase App/functions/index.js
+++ FireBase App/functions/index.js
@@ -1,24 +1,24 @@
-const functions = require('firebase-functions');
-// const firebase = require('firebase-admin');
-const express = require ('express');
-const engines = require('consolidate');
+// const functions = require('firebase-functions');
+// // const firebase = require('firebase-admin');
+// const express = require ('express');
+// const engines = require('consolidate');
 
-// const firebaseApp = firebase.initializeApp(
-//     functions.config().firebase
-// );
-
-
-const app = express();
-app.engine('hbs', engines.handlebars);
-app.set('views', './views');
-app.set('view engine', 'hbs');
+// // const firebaseApp = firebase.initializeApp(
+// //     functions.config().firebase
+// // );
 
 
-app.get('/api/login', (request, response) => 
-{ 
-    response.send(`${Date.now()}`);
-});
+// const app = express();
+// app.engine('hbs', engines.handlebars);
+// app.set('views', './views');
+// app.set('view engine', 'hbs');
+
+
+// app.get('/api/login', (request, response) => 
+// { 
+//     response.send(`${Date.now()}`);
+// });
 
 
 
-exports.app = functions.region('us-east1').https.onRequest(app);
+// exports.app = functions.region('us-east1').https.onRequest(app);
